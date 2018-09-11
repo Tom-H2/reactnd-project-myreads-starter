1588ef860aa1874d821ceb1f53e0fe3bdf2e5263
Thomas Harris <lostmt.tom@gmail.com>
2018-09-10 18:50:30 -0700

    feat: Imports Books Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
+import Books from './components/Books'
+
 class BooksApp extends React.Component {
   state = {
     /**
@@ -38,34 +40,21 @@ class BooksApp extends React.Component {
             </div>
           </div>
         ) : (
+          //Start of Books Page
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
+
+
+{/*Currently Reading Shelf start*/}
             <div className="list-books-content">
               <div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
-                        </div>
-                      </li>
+                      <Books />
                       <li>
                         <div className="book">
                           <div className="book-top">
@@ -87,6 +76,9 @@ class BooksApp extends React.Component {
                     </ol>
                   </div>
                 </div>
+
+
+{/*Want to read bookshelf start*/}
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
@@ -130,6 +122,8 @@ class BooksApp extends React.Component {
                     </ol>
                   </div>
                 </div>
+
+{/*Read bookshelf start*/}
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
