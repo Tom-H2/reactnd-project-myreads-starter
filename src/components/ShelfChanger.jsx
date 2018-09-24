c6b94752d09d18b8dceda5aa369fffe9ced88079
Thomas Harris <lostmt.tom@gmail.com>
2018-09-23 22:00:36 -0700

    docs: Writes pseudo code as start to ShelfChanger functionality

--- src/components/ShelfChanger.jsx
+++ src/components/ShelfChanger.jsx
@@ -1,6 +1,12 @@
+//Currently Reading, Want to Read, and Read shelves
+//Makes component to render books from API to selected shelves
+//imports React
 import React, { Component } from 'react'
 
-export default class Book extends Component {
+import Books from './Books'
+
+class shelfChanger extends Component {
+  //use map() to sort through BooksAPI 
   render() {
   return (
     <div>
@@ -9,3 +15,4 @@ export default class Book extends Component {
   )
   }
 }
+export default shelfChanger
