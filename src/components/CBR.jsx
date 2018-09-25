7e0a70707dde0fecbcec61d9a2979145ec6d5910
Thomas Harris <lostmt.tom@gmail.com>
2018-09-24 18:56:56 -0700

    feat: Creates component template

--- src/components/CBR.jsx
+++ src/components/CBR.jsx
@@ -1,28 +1,12 @@
 import React, { Component } from 'react'
 
-export default class Book extends Component {
+class CBR extends Component {
   render() {
-  return (
-    <div>
-    <li>
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-          <div className="book-shelf-changer">
-            <select>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
-        </div>
-        <div className="book-title">Ender's Game</div>
-        <div className="book-authors">Orson Scott Card</div>
+    return (
+      <div>
+        //This is a template for creating components
       </div>
-    </li>
-    </div>
-  )
+    )
   }
 }
+export default CBR
