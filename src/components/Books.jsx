cc6c0e6e191672b46e7901156d9d215f87fdbe39
Thomas Harris <lostmt.tom@gmail.com>
2018-09-30 11:13:57 -0700

    refactor: removes select for drop down menu to change shelves and places in ChangeShelf

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,23 +1,18 @@
 //Component to get API data about individual books
 
 import React, { Component } from 'react'
-
+import ChangeShelf from './ChangeShelf'
 export default class Book extends Component {
   render() {
   return (
     <li>
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:this.props.coverImage }}></div>
-          <div className = 'change-shlef'>
-            <select>
-            {/*<onClick{() => props.onchangeShelf(move))} />*/}
-              <option value="move">Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.coverImage})` }}></div>
+          <div className = 'change-shelf-changer'>
+            <ChangeShelf
+              onChange= {this.handleChange}
+            />
           </div>
         </div>
         <div className="book-title">{this.props.title}</div>
