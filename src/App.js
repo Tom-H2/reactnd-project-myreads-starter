0f7ed39e9f191f5c1e10486e3097ffc30b44a1cd
Thomas Harris <lostmt.tom@gmail.com>
2018-09-30 11:10:45 -0700

    docs: Uncomments component imports

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css'
 
 import Books from './components/Books'
 import BookShelf from './components/BookShelf'
-//import ChangeShelf from './components/ChangeShelf'
+import ChangeShelf from './components/ChangeShelf'
 //import Search from './components/Search'
 
 
@@ -53,9 +53,7 @@ class BooksApp extends React.Component {
 
   render() {
 
-    let Books = [
-
-    ];
+    let Books = [];
     //{title:"To Kill A Mockingbird", author:"Harper Lee", coverImage:'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' },
     //{title: "Ender's Game", author:"Orson Scott Card", coverImage:'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }
     // <Books title="1776" author="David McCullough" coverImage='url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' />
@@ -72,14 +70,6 @@ class BooksApp extends React.Component {
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                 <input
                   type="text"
                   placeholder="Search by title or author"
