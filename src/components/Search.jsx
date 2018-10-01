e8cbd6d7589bac1425a54e3002587936fb9cf3cd
Thomas Harris <lostmt.tom@gmail.com>
2018-09-30 21:50:23 -0700

    refactor: Cleans up code and attempts to complete component which is not yet working

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,52 +6,54 @@ import React, {Component} from 'react'
 
 // method to update books
 // Book component  - displays thumbnail image, title, authors(NEED WORK), picklist
-class SearchBook extends Component {
-
+class Search extends Component {
 	// method to check if search results are already on a shelf
-    /*checkShelf = (searchResults, books) => {
-    	searchResults.map(result =>
-	    	// compare result.id against each current book.if
-	      	// if a match, change the result.shelf = book.shelf
-	      	// if no match, set to "none"
-	      	// need to initialize shelf to "none"
-	      	result.shelf = "none"
+ //open search query
 
-	      	this.books.map(book => (
-	        	(book.id === result.id) ? result.shelf = book.shelf : "none"
-	      	))
-	      	console.log(result.shelf)
-	      	this.setState({books}) // TESTING - REMOVE - OK HERE
-	    )
-	}*/
+	constructor(props) {
+    super(props);
+    this.state = (query);
+	}
+
+	updateQuery = (query) => {
+		this.setState({query})
+		this.runSearch(query)
+	}
+
+	runSearch = (query) => {
+		BooksAPI.search(query).then((books => {
+			this.setState({foundBooks:books})
+		}))
+	}
 
 	render() {
-		//checkShelf(this.props.searchResults, this.props.books)
 
 		return (
-			<div className="search-books">
-        <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-          <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
+			<div className="app">
+        {this.state.showSearchPage ? (
+          <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <div className="search-books-input-wrapper">
+                <input
+                  type="text"
+                  placeholder="Search by title or author"
+                  value={this.state.query}
+                  onChange={(event) => this.updateQuery(event.target.value)}
+                />
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+              <BookShelf shelfName='found' books={this.state.foundBooks}/>
+            </div>
           </div>
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
-          </div>
-        </div>
-      </div>
+        ) : (
+          <div className="list-books"></div>
         )
-    }
+
+	}
+	</div>
+ )
 }
-
-
-export default SearchBook
+export default Search;
