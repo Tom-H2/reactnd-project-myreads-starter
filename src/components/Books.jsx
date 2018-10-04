0ad3ee4e350728180df7bf9c5d439d9a3ef3e731
Thomas Harris <lostmt.tom@gmail.com>
2018-10-04 16:22:44 -0700

    feat: Copies code for select menu back into component

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -4,15 +4,25 @@ import React, { Component } from 'react'
 import ChangeShelf from './ChangeShelf'
 export default class Book extends Component {
   render() {
+    let coverImage = this.prop.book.imageLinks ?
+      this.props.book.imageLinks.thumbnail : ''
   return (
     <li>
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.coverImage})` }}></div>
           <div className = 'change-shelf-changer'>
-            <ChangeShelf
-              onChange= {this.handleChange}
-            />
+            <select onChange={(e) => this.props.changeShelf(
+                this.props.book, e.target.value
+            )}
+                value={this.props.currentShelf}>
+                
+              <option value="move">Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
           </div>
         </div>
         <div className="book-title">{this.props.title}</div>
