ecb189f3d67e793d0c84a1c9286839d6ff612720
Thomas Harris <lostmt.tom@gmail.com>
2018-10-04 16:24:54 -0700

    feat: Updates onChange function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,10 @@
-//For this code I recieved help from Susan Pommer who is a classmate in the Grow with Google Udacity FEND cohort
+
 // Display book image, title, author to page
+//This is currently not functional and the call in App.js is commented out
 
 // import React
-import React, {Component} from 'react'
-
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 class Search extends Component {
 
 	constructor(props) {
@@ -15,30 +16,12 @@ class Search extends Component {
 		};
 	 }
 
- componentDidMount() { //instantiates network request
-	 BooksAPI.getAll().then((books => {
-		 this.setState ({books:books})
-		 console.log(books); //shows books array in console
-	 }))
-  }
-
-	updateQuery = (query) => {
-		this.setState({query})
-		this.runSearch(query)
-	}
-
-	runSearch = (query) => {
-		BooksAPI.search(query).then((books => {
-			this.setState({foundBooks:books})
-		}))
-	}
-
 	render() {
 
 		return (
           <div className="search-books">
             <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <link className="close-search" to="/">Close</link>
               <div className="search-books-input-wrapper">
                 <input
                   type="text"
@@ -50,10 +33,9 @@ class Search extends Component {
             </div>
             <div className="search-books-results">
               <ol className="books-grid"></ol>
-              <BookShelf shelfName='found' books={this.state.foundBooks}/>
             </div>
           </div>
-        )
+        );
 	}
-
+}
 export default Search;
