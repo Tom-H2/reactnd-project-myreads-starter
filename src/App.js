e30be4d9f07ec6809231052b5ff53b6b8b72dcc4
Thomas Harris <lostmt.tom@gmail.com>
2018-10-07 10:06:34 -0700

    docs: Comments about role of App.js as the frame for the components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 //import React libraries
 import React from 'react'
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
 import { Route } from 'react-router-dom'
-//imports API
 //import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-import Books from './components/Books'
-import BookShelf from './components/BookShelf'
+//import Books from './components/Books'
+//import BookShelf from './components/BookShelf'
 //import ChangeShelf from './components/ChangeShelf'
-import Search from './components/Search'
-import BookCase from './components/BookCase'
+import Search from './components/Search';
+import BookCase from './components/BookCase';
 
-
+//App.js holds the components which are the two pages in the app. The main BookCase and the search page
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-     books: [], //creats open array for storing books
-     query: [], //open search query
-     move: [],
-     foundBooks: [],
-     showSearchPage: false
-  }
-
   render() {
     return (
       <div>
@@ -35,16 +20,6 @@ class BooksApp extends React.Component {
         <Route exact path="/" component= { Search } />
       </div>
     );
-
-      /*TODO This isn't correct but I copies everything over to Search.jsx so both files have the same code fom here down.
-        <Search
-        onChange={(event) => this.updateQuery(event.target.value)}
-         <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>/>
-      */
-
-      <div>
-        App
-      </div>
   }
 }
 
