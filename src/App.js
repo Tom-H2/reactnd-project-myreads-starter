453207b5eba25c98696305b947f04e6bbcafca17
Thomas Harris <lostmt.tom@gmail.com>
2018-10-10 18:24:35 -0700

    chore: Changes paths to search page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import './App.css'
-
-//import Books from './components/Books'
-//import BookShelf from './components/BookShelf'
-//import ChangeShelf from './components/ChangeShelf'
 import Search from './components/Search';
 import BookCase from './components/BookCase';
-
+//TODO Fix the path to the search component
 //App.js holds the components which are the two pages in the app. The main BookCase and the search page
 class App extends React.Component {
 
@@ -18,7 +14,7 @@ class App extends React.Component {
     return (
       <div>
         <Route exact path="/" component= { BookCase } />
-        <Route exact path="/" component= { Search } />
+        <Route exact path="/Search" component= { Search } />
       </div>
     );
   }
